4d58303fb0da927bb9375e602f7416e32916331c
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-09 12:27:57 -0500

    Lesson 19: toggle switch implementation is done except for the platform

--- Assets/Scripts/ToggleSwitch.cs
+++ Assets/Scripts/ToggleSwitch.cs
@@ -16,18 +16,34 @@ public class ToggleSwitch : MonoBehaviour
    {
       _spriteRenderer = GetComponent<SpriteRenderer>();
    }
-
-   void OnTriggerEnter2D(Collider2D collision)
+   
+   //runs every frame a collider is inside of this collider
+   void OnTriggerStay2D(Collider2D collision)
    {
       var player = collision.GetComponent<Player>(); 
       if (player == null)
          return;
-      
+
+      //check for the existence of a rigidbody 
+      var playerRigidbody = player.GetComponent<Rigidbody2D>(); 
+      if(playerRigidbody == null)
+         return;
+
       //is the position of the incoming collider further to the right on the x axis than the position the collider?
       bool wasOnRight = collision.transform.position.x > transform.position.x;
       
+      //is the player walking to the right? positive x velocity is to the right
+      var velocity = playerRigidbody.velocity;
+      bool playerWalkingRight = velocity.x > 0;
+      
+      //is the player walking to the left?
+      bool playerWalkingLeft = velocity.x < 0; 
       //set the sprite of the sprite renderer to the left or right switch sprite
-      //if wasOnRight is true, set the sprite to toggleSwitchRight, otherwise, set it to toggleSwitchLeft
-      _spriteRenderer.sprite = wasOnRight ? _toggleSwitchLeft : _toggleSwitchRight; 
+      //if the player is on the right and they are walking right, use the right switch
+      //if the player is on the left and walking left, use the left switch
+      if (wasOnRight && playerWalkingRight)
+         _spriteRenderer.sprite = _toggleSwitchRight; 
+      else if (wasOnRight == false && playerWalkingLeft)
+         _spriteRenderer.sprite = _toggleSwitchLeft; 
    } 
 }
