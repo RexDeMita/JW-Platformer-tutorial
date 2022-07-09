09f2249ddaf2d1a11a8bb5dd0c0184f8210e0b29
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-09 14:29:49 -0500

    Lesson 20: made prefabs

---
+++ Assets/Scripts/Lesson notes/L20Prefabs.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Lesson_notes
+{
+    public class L20Prefabs : MonoBehaviour
+    {
+        /*
+         prefabs should just work in the scene without much more code or work
+            ideally only referencing itself or its own components
+            if the object references other objects in the scene, that is more likely a specific scenario that needs its own configuring
+            
+            something too simple would also not make a good prefab
+            
+            
+         */
+    }
+}
