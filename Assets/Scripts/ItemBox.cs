e3d4ea9d3101ca3edb1e5331711584802437be2b
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-09 15:56:01 -0500

    Lesson 20: itembox can spawn prefabs

--- Assets/Scripts/ItemBox.cs
+++ Assets/Scripts/ItemBox.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class ItemBox : HittableFromBelow
 {
+    [SerializeField] GameObject _itemPrefab; 
     [SerializeField] GameObject _item;
     [SerializeField] Vector2 _itemLaunchVelocity;
     bool _used;
@@ -18,11 +19,13 @@ public class ItemBox : HittableFromBelow
     
     //this property will override CanUse in the base class
     //if the item was used, CanUse will become true
-    protected override bool CanUse => _used == false && _item != null;  
+    protected override bool CanUse => _used == false;  
     
     //this method will override the Use method in the base class
     protected override void Use()
     {
+        //the itemPrefab selected in the inspector is assigned to the item field
+        _item = Instantiate(_itemPrefab, transform.position + Vector3.up, Quaternion.identity, transform); 
         //backup check in case the CanUse check fails
         if (_item == null)
             return;
