af0c45df0f270f2872e58498c31934c2101e1c31
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-11 12:53:00 -0500

    Lesson 21: new UI Pack, added panel graphic, and questionable onValidate

--- Assets/Scripts/Lesson notes/L21MainMenuUI.cs
+++ Assets/Scripts/Lesson notes/L21MainMenuUI.cs
@@ -8,6 +8,10 @@ public class L21MainMenuUI : MonoBehaviour
     TMP: when auto size is checked, you can change the font size with the auto size options and it will resize based on the size of the box surrounding the text
         stretch images along the bottom of a canvas by anchoring and stretching in the anchor presets window
         
+        text can be given spacing
+            in rect transform
+            the left, top, right, and bottom fields are for the number of pixels to move the text inward
+            
     Set native size 
         reproduces the sprite in the native aspect ratio
         
@@ -18,6 +22,6 @@ public class L21MainMenuUI : MonoBehaviour
     layout group components for UI elements
         different ways of auto spacing UI elements within each other
         
-        
+    
      */
 }
--- Assets/Scripts/UIStartLevelButton.cs
+++ Assets/Scripts/UIStartLevelButton.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,6 +11,14 @@ public class UIStartLevelButton : MonoBehaviour
 
     public void LoadLevel()
     {
+        //Loads the level given in the inspector
         SceneManager.LoadScene(_levelName); 
+    }
+
+    void OnValidate()
+    {
+        //allows us to see our input in the inspector become a change to the text in the scene
+        //we now know what level corresponds to the level we are writing the level name inside of
+        GetComponentInChildren<TMP_Text>()?.SetText(_levelName);
     }
 }
