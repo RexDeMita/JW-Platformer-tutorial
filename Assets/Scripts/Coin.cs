760ebf99eae0d2beac1449869dac6ebd9ad4e103
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-11 20:12:35 -0500

    Lesson 23: added coin audio and coin sounds

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -17,17 +17,22 @@ public class Coin : MonoBehaviour
         var player = collision.GetComponent<Player>();
         //Debug.Log("Player");
         if (player == null)
-            return; 
-        
-        //deactivate the object
-        gameObject.SetActive(false);
+            return;
 
         //increment the number of coins collected 
         CoinsCollected++;
         
-        //print the value of coinscollected to the log
+        //print the value of coins collected to the log
         Debug.Log(CoinsCollected);
         
+        //add 100 to the score
         ScoreSystem.Add(100);
+        
+        //Turn off the sprite renderer and collider
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false; 
+        
+        //getting a reference to the audio source and then using it right away to play the sound
+        GetComponent<AudioSource>().Play();
     }
 }
---
+++ Assets/Scripts/Lesson notes/L23AudioandMusic.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Lesson_notes
+{
+    public class L23AudioandMusic : MonoBehaviour
+    {
+    /*
+     dragging an audio clip from the project view into the inspector will create an audio source component
+     
+     
+     */
+    }
+}
