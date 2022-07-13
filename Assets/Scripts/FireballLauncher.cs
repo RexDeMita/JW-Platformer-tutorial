a0b690aa03318facd45a6253c508950f0ea61bbd
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-13 11:12:28 -0500

    Lesson 24: reading input to launch Fireballs

--- Assets/Scripts/FireballLauncher.cs
+++ Assets/Scripts/FireballLauncher.cs
@@ -5,16 +5,35 @@ using UnityEngine;
 
 public class FireballLauncher : MonoBehaviour
 {
-    [SerializeField] Fireball _fireballPrefab; 
+    [SerializeField] Fireball _fireballPrefab;
+    
+    string _fireButton;
+    int _playerNumber;
+    private Player _player;
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        Instantiate(_fireballPrefab, transform.position, Quaternion.identity); 
+        //get the reference to the player
+        _player = GetComponent<Player>();
+        
+        //cache the value for the player number
+        _playerNumber = _player.PlayerNumber;
+        
+        //cache the string for fireButton
+        _fireButton = $"P{_playerNumber}Fire"; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if the fireButton input was held during this frame
+        if (Input.GetButtonDown(_fireButton))
+        {
+            //instantiate the fireball prefab
+            Instantiate(_fireballPrefab, transform.position, Quaternion.identity);    
+        }
         
     }
 }
