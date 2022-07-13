f3a7dffe56d460afb052078e0fcf5e7a8af7cb0a
Mita Rakotomahenina <mita.rakotoma@gmail.com>
2022-07-13 11:50:18 -0500

    Lesson 24: read Fireball directions and limited the number of bounces

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Fireball : MonoBehaviour
 {
+    [SerializeField] float _launchForce = 5f;
+    [SerializeField] float _bounceForce = 5f;
+    [SerializeField] int _bouncesRemaining = 3;
+    public float Direction { get; set; }
     Rigidbody2D _rigidbody;
-    [SerializeField] float _launchForce = 5;
-
-    // Start is called before the first frame update
+   
     void Start()
     {
+        //rigidbody reference so we can send the fireballs in the direction dictated by the player
         _rigidbody = GetComponent<Rigidbody2D>();
-        _rigidbody.velocity = Vector2.right * _launchForce;
-    }
+        _rigidbody.velocity = new Vector2(_launchForce * Direction, _bounceForce); 
+    } 
 
-    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //decrement bounces remaining when the fireball collides with anything
+        _bouncesRemaining--;
+        
+        //if bounces remaining goes below zero, destroy the fireball
+        //we want 3 bounces then die on the 4th bounce
+        if(_bouncesRemaining < 0)
+            Destroy(gameObject);
+        else
+        {
+            //this maintains the velocity after each bounce
+            _rigidbody.velocity = new Vector2(_launchForce * Direction, _bounceForce); 
+        }
+    }
 }
--- Assets/Scripts/FireballLauncher.cs
+++ Assets/Scripts/FireballLauncher.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class FireballLauncher : MonoBehaviour
 {
     [SerializeField] Fireball _fireballPrefab;
+    [SerializeField] float _fireRate = 0.25f;
     
     string _fireButton;
     int _playerNumber;
-    private Player _player;
+    Player _player;
+    float _nextFireTime;
+    string _horizontalAxis;
 
 
     // Start is called before the first frame update
@@ -23,16 +26,29 @@ public class FireballLauncher : MonoBehaviour
         
         //cache the string for fireButton
         _fireButton = $"P{_playerNumber}Fire"; 
+        
+        //cached value for horizontal input
+        _horizontalAxis = $"P{_playerNumber}Horizontal"; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if the fireButton input was held during this frame
-        if (Input.GetButtonDown(_fireButton))
+        //if the fireButton input was held during this frame and Time.time is greater than the delay we give between each shot
+        if (Input.GetButtonDown(_fireButton) && Time.time >= _nextFireTime)
         {
-            //instantiate the fireball prefab
-            Instantiate(_fireballPrefab, transform.position, Quaternion.identity);    
+            //reference to the horizontal input
+            var horizontal = Input.GetAxis(_horizontalAxis);
+            
+            //instantiate the fireball prefab and get a reference to the prefab
+            Fireball fireball = Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+            
+            //set the direction of the fireball object based on the input
+            fireball.Direction = horizontal >= 0 ? 1f : -1f; 
+            
+            //this will allow for some time in between shots
+            _nextFireTime += Time.time + _fireRate; 
+
         }
         
     }
